37e55c42b68becc96ec09055a09d15f23ff6dce0
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-10-25 14:24:10 -0700

    updates

--- player_controls.gd
+++ player_controls.gd
@@ -1,15 +1,20 @@
 extends Camera2D
 
-var zooming_speed = 1.1
+var zooming_speed = 1.05
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Movement kets
+	if Input.is_action_pressed("foward"):
+		self.position.y = self.position.y - 5
+	elif Input.is_action_pressed("backward"):
+		self.position.y = self.position.y + 5
+	elif Input.is_action_pressed("left"):
+		self.position.x = self.position.x - 5
+	elif Input.is_action_pressed("right"):
+		self.position.x = self.position.x + 5
 
 func _input(event):
 	# Scrolling
@@ -21,6 +26,3 @@ func _input(event):
 	elif event.is_action("esc"):
 			#get_tree().reload_current_scene() #reset
 			get_tree().quit() #quit
-	# Movement Keys
-	elif event.is_action_pressed("foward"):
-		self.position.y = self.position.y - 5
--- zone_command.gd
+++ zone_command.gd
@@ -1,17 +1,35 @@
 extends Node2D
 
-@onready var my_polygon = $Area2D/CollisionPolygon2D
-@onready var point_mesh = $MeshInstance2D
+@onready var col_poly = $Area2D/CollisionPolygon2D
+@onready var mesh_poly = $Polygon2D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Left_Mouse"):
 		var click_pos = get_local_mouse_position()
 			
-		if my_polygon.polygon.size() >= 3:
-			var point_index = 2  
+		if col_poly.polygon.size() >= 3:
+			#var point_index = 2  
 			
-			var polygon = my_polygon.polygon
-			polygon[point_index] = click_pos
-			my_polygon.polygon = polygon
-			
-			point_mesh.global_position = click_pos
+			#var polygon = col_poly.polygon
+			#polygon[point_index] = click_pos
+			#col_poly.polygon = polygon
+			update_polygon_point(mesh_poly, 2, click_pos)
+	elif Input.is_action_just_released("Left_Mouse"):
+		var click_pos1 = get_local_mouse_position()
+		update_polygon_point(mesh_poly, 0, click_pos1)
+
+func update_polygon_point(sel_poly, point_index, click_pos):
+	if sel_poly.polygon.size() >= 3:
+		var polygon = sel_poly.polygon
+		
+		var new_width = abs(polygon[2].x - click_pos.x)
+		var new_height = abs(polygon[2].y - click_pos.y)
+		
+		polygon[3].x = -click_pos.x - new_width
+		polygon[3].y = -click_pos.y
+		polygon[1].x = -polygon[2].x
+		polygon[1].y = -click_pos.y + new_height
+		polygon[3].x = -click_pos.x
+		polygon[3].y = -polygon[0].y
+		
+		sel_poly.polygon = polygon
